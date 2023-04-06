d21207e2bb7e95cf251443c3f41605f84e7906dc
daniellamberti <daniellamberti@gmail.com>
2023-04-06 16:12:23 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let pasww = "asdf";
+i = 1;
+let cont;
+ 
+
+while (i <= 3) {
+    let contra = (prompt(alert("Ingrese su contraseña")));
+
+
+    if(contra != pasww) {
+        alert("Intento número " + i + " de LOGIN");
+        i++
+
+    }
+    else {
+        alert("Su contraseña es correcta, ha ingresado a su LOGIN");
+        cont = si;
+        break;
+    }
+}
+    alert("Ha utilizado sus 3 intentos de LOGIN");
